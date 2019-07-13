4664afbf28c121015c809d149e2883d0b55569be
adhityaramadhanus <adhitya.ramadhanus@gmail.com>
2019-07-13 16:29:41 +0700

    add domain entity ability

--- ability.go
+++ ability.go
@@ -1,9 +1,21 @@
 package godex
 
+import "fmt"
+
 type Ability struct {
-	ID          int
-	Name        string
-	Description string
+	ID      int
+	Name    string
+	Effects []string
+}
+
+func (a Ability) String() string {
+	format := "%s\n%s"
+
+	var effects string
+	for _, effect := range a.Effects {
+		effects += fmt.Sprintf("- %s\n", effect)
+	}
+	return fmt.Sprintf(format, a.Name, effects)
 }
 
 type Abilities []Ability
